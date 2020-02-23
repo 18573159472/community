80aa4efd6469388ca8a5ff8af081b9b72002ccb1
pangs <604648413@qq.com>
2020-02-23 19:31:38 +0800

    add index

---
+++ src/main/java/life/pangs/communitydev/controller/IndexController.java
@@ -0,0 +1,15 @@
+package life.pangs.communitydev.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping("/")
+    public String index2Hello(Model model){
+        return "index";
+    }
+}
