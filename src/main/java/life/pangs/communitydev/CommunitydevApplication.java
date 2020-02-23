b3392d59b2934f8b62d4545ae7b85fdab8d74e80
pangs <604648413@qq.com>
2020-02-23 15:49:32 +0800

    init

---
+++ src/main/java/life/pangs/communitydev/CommunitydevApplication.java
@@ -0,0 +1,13 @@
+package life.pangs.communitydev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunitydevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunitydevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/life/pangs/communitydev/CommunitydevApplicationTests.java
@@ -0,0 +1,13 @@
+package life.pangs.communitydev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunitydevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
